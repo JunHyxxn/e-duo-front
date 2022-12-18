1b94d58b80173a3f1b036edd4007a4e2673cf274
황준현 <ihrug000@gmail.com>
2022-12-19 00:11:31 +0900

    :sparkles: feat: 아이콘 추가

--- src/api/design/fontAwesomeIcon.js
+++ src/api/design/fontAwesomeIcon.js
@@ -2,12 +2,23 @@ import Vue from "vue";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faCheckCircle } from "@fortawesome/free-regular-svg-icons";
 import {
   faPlus,
   faMinus,
   faUserPlus,
   faPeopleLine,
+  faCircleCheck,
+  faUserGroup,
 } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faPlus, faMinus, faUserPlus, faPeopleLine);
+library.add(
+  faPlus,
+  faMinus,
+  faUserPlus,
+  faPeopleLine,
+  faCircleCheck,
+  faCheckCircle,
+  faUserGroup
+);
 Vue.component("font-awesome-icon", FontAwesomeIcon);
